76310a05cbc29a8f76a631d711c41d925afb0ec1
Łukasz <lukaszkogut1996@gmail.com>
2023-05-17 19:55:36 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
-
-import { Component } from "react";
+import { Component } from 'react';
 
 export class App extends Component {
   state = {
@@ -7,4 +6,26 @@ export class App extends Component {
     neutral: 0,
     bad: 0,
   };
+
+  updateStateCount = stateCount => {
+    this.setState(prevState => ({ [stateCount]: prevState[stateCount] + 1 }));
+  };
+  render() {
+    return (
+      <container>
+        {' '}
+        <section title=" Please leave feedback">
+          <button onClick={() => this.updateStateCount('good')}>good</button>
+          <button onClick={() => this.updateStateCount('neutral')}>
+            neutral
+          </button>
+          <button onClick={() => this.updateStateCount('bad')}>bad</button>
+        {/* {this.state.good}
+        {this.state.neutral}
+        {this.state.bad} */}
+        </section>
+      </container>
+    );
+  }
 }
+
